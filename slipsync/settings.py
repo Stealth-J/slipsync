a3e62530bb3e5db42d6530a3cd49c30ab0569a7c
Stealth-J <nosayamejeffrey@gmail.com>
2025-09-01 16:34:07 +0100

    removing the key

--- slipsync/settings.py
+++ slipsync/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3pe--a(jf0gzkqtsor2fc0@i3gv_3v@^utq8yxp!7)j2#18e8n'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "slipsync-secret-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
